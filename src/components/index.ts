17b364c45feda9d40ddb044c01f668ff355637f6
Agions <1051736049@qq.com>
2026-03-21 22:46:51 +0800

    refactor: 添加组件和composables导出索引
    - 添加 components/index.ts 统一导出所有组件
    - 添加 composables/index.ts 统一导出composables和类型

---
+++ src/components/index.ts
@@ -0,0 +1,18 @@
+// Common Components
+export { default as Button } from './common/Button.vue'
+export { default as Modal } from './common/Modal.vue'
+export { default as Tooltip } from './common/Tooltip.vue'
+export { default as KeyboardShortcutsHelp } from './common/KeyboardShortcutsHelp.vue'
+
+// Layout Components
+export { default as ToolBar } from './layout/ToolBar.vue'
+export { default as SidePanel } from './layout/SidePanel.vue'
+export { default as VideoPreview } from './layout/VideoPreview.vue'
+export { default as SubtitleList } from './layout/SubtitleList.vue'
+export { default as StatusBar } from './layout/StatusBar.vue'
+export { default as SettingsView } from './layout/SettingsView.vue'
+export { default as BatchProcessView } from './layout/BatchProcessView.vue'
+
+// Video Components
+export { default as ROISelector } from './video/ROISelector.vue'
+export { default as Timeline } from './video/Timeline.vue'
---
+++ src/composables/index.ts
@@ -0,0 +1,13 @@
+// Composables
+export { useVideoPlayer } from './useVideoPlayer'
+export { useOCREngine } from './useOCREngine'
+export { useFileOperations } from './useFileOperations'
+export { useSubtitleExtractor } from './useSubtitleExtractor'
+export { useKeyboardShortcuts } from './useKeyboardShortcuts'
+export { useBatchProcessor } from './useBatchProcessor'
+export { useTheme } from './useTheme'
+
+// Types
+export type { KeyboardShortcut } from './useKeyboardShortcuts'
+export type { BatchJob, BatchOptions } from './useBatchProcessor'
+export type { OCRResult } from './useOCREngine'
